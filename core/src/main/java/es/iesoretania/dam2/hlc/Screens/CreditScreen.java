4331b6886a76bcb9d3e81040bb7c787bafcebb4c
J0tabe93 <joseberruizcasado@gmail.com>
2023-01-12 14:02:57 +0100

    Añadida CreditScreen con los creditos, añadido en el README.md todos los creditos que faltaban, ya no se puede redimensionar la ventana del juego, ajustes menores.

--- core/src/main/java/es/iesoretania/dam2/hlc/Screens/CreditScreen.java
+++ core/src/main/java/es/iesoretania/dam2/hlc/Screens/CreditScreen.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import es.iesoretania.dam2.hlc.Game.OneTaps2D;
